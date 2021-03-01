a962e841a582e6a8efe2ef25d8bb028ef509a023
Steven Yan <ssyan@ualberta.ca>
2021-02-28 23:42:57 -0700

    added min_rating endpoint

--- FlaskAPI/boardGameData.py
+++ FlaskAPI/boardGameData.py
@@ -7,18 +7,18 @@ def get_boardgames(game_params):
     min_rating = game_params['min_rating']
 
     atlasURL = 'https://api.boardgameatlas.com/api/search?player_counts=' + max_players + \
-        '&lt_max_playtime=' + max_playtime + '&client_id=Q0gY3mITdP'
+        '&lt_max_playtime=' + max_playtime + \
+        '&gt_average_user_rating=' + min_rating + '&client_id=Q0gY3mITdP'
     atlasResponse = requests.get(atlasURL)
     atlasJson = atlasResponse.json()
     
     results = {}
     for game in atlasJson['games']:
-        if float(game['average_user_rating']) >= float(min_rating):
-            results[game['name']] = {
-                'min_players': game['min_players'],
-                'max_players': game['max_players'],
-                'min_playtime': game['min_playtime'],
-                'max_playtime': game['max_playtime'],
-                'rating': game['average_user_rating']
-                }
+        results[game['name']] = {
+            'min_players': game['min_players'],
+            'max_players': game['max_players'],
+            'min_playtime': game['min_playtime'],
+            'max_playtime': game['max_playtime'],
+            'rating': game['average_user_rating']
+            }
     return results
