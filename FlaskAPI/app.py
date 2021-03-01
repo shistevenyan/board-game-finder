f70100bee0317f02236473eb9bef407915623f23
KenjiAu-Creator <58325531+KenjiAu-Creator@users.noreply.github.com>
2021-02-28 23:21:53 -0700

    Initial commit with flask;

---
+++ FlaskAPI/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, request
+from flask_cors import CORS, cross_origin
+from boardGameData import *
+
+app = Flask(__name__)
+CORS(app, support_credentials=True)
+
+@app.route('/boardgame-result', methods=['GET'])
+@cross_origin(support_credentials=True)
+
+def boardgame_result():
+    if request.method == "GET":
+        game_params = {}
+        game_param_player = request.args.get('max_players')
+        game_param_playtime = request.args.get('max_playtime')
+        game_param_rating = request.args.get('min_rating')
+        game_params['max_players'] = game_param_player
+        game_params['max_playtime'] = game_param_playtime
+        game_params['min_rating'] = game_param_rating
+        if game_params:
+            return get_boardgames(game_params)
+        return {"results": [], "error_msg": "Bad data"}, 400
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ FlaskAPI/boardGameData.py
@@ -0,0 +1,24 @@
+import requests
+import random
+client_id = 'Q0gY3mITdP'
+
+def get_boardgames(game_params):
+    max_players = game_params['max_players']
+    max_playtime = game_params['max_playtime']
+    min_rating = game_params['min_rating']
+
+    atlasURL = 'https://api.boardgameatlas.com/api/search?player_counts={max_players}&lt_max_playtime={max_playtime}&client_id={client_id}'
+    atlasResponse = requests.get(atlasURL)
+    atlasJson = atlasResponse.json()
+    
+    results = {}
+    for game in atlasJson['games']:
+        if float(game['average_user_rating']) >= float(min_rating):
+            results[game['name']] = {
+                'min_players': game['min_players'],
+                'max_players': game['max_players'],
+                'min_playtime': game['min_playtime'],
+                'max_playtime': game['max_playtime'],
+                'rating': game['average_user_rating']
+                }
+    return results
