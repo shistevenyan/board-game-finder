f6fc096bb27536a6e00258760689b9de55b42a0d
Steven Yan <ssyan@ualberta.ca>
2021-03-04 17:58:05 -0700

    finished layout of home

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,63 +1,85 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import AppLoading from 'expo-app-loading';
+import { useFonts, Inter_400Regular } from '@expo-google-fonts/inter';
+import Logo from '../assets/BoardLogo.png'; 
+import { Button } from 'react-native-elements';
 
-// css stylesheet for React Native
-const styles = StyleSheet.create({
-    title: {
-        fontSize: 100,
-        color: "#FF6767",
-        fontWeight: "400",
-    },
-    subTitle: {
-        color: "#FF6767",
-        fontSize: 15,
-        fontWeight: "400",
-    },
-    content: {
-        color: "#FF6767",
-        fontSize: 15,
-        width: 300,
-        marginBottom: 50,
-        fontWeight: "400",
-        
-    },
-    button: {
-        backgroundColor: "#FF6767",
-        color: "white",
-        fontSize: 20,
-        borderRadius: 15,
-        width: 200,
-        height: 70,
-        fontWeight: "400",
-    }
-})
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 // Name of component
 const Home = () => {
     // Logic for components can go in here.
+    let [fontsLoaded] = useFonts({
+        Inter_400Regular,
+    });
+
+    if (!fontsLoaded) {
+        return <AppLoading />;
+    }
 
     return (
-        <View>
+        <View style={styles.container}>
             {/*
             View is a container component
             Use Text to encapsulate strings instead of usual HTML tags.
             */}
-            <Text style={styles.title}>
-                Board.
-            </Text>
-            <Text style={styles.subTitle}>
-                Board Game Finder
-            </Text>
+            <Image source={Logo} style={styles.logo}/>
             <Text style={styles.content}>
                 Ever have a hard time choosing which board game to play? Have no fear, we are here to help! Let us recommend you a few to play!
             </Text>
 
-            <Text style={styles.button}>
-                Get Started
-            </Text>
+            <Button
+                buttonStyle={styles.button}
+                titleStyle={styles.buttonTitle}
+                icon={
+                    <Icon
+                        name="navigate-next"
+                        size={40}
+                        color="white"
+                    />
+                }
+                iconRight
+                title="Let's Get Started"
+            />
         </View>
-    )
-}
+    );
+};
 
 // Make sure there is a export with the component name.
 export default Home;
+
+// css stylesheet for React Native
+const styles = StyleSheet.create({
+    container: {
+        flex: 1, 
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+
+    logo: {
+        marginBottom: 50,
+    },
+
+    content: {
+        color: "#FF6767",
+        fontSize: 15,
+        width: 300,
+        marginBottom: 70,
+        textAlign: 'center',
+        fontFamily: "Inter_400Regular",
+        fontWeight: "400",
+    },
+
+    button: {
+        backgroundColor: "#FF6767",
+        borderRadius: 60,
+        width: 200,
+        height: 55,
+    },
+
+    buttonTitle: {
+        fontFamily: "Inter_400Regular",
+    }
+
+})
